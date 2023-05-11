24c2952f8cc700f9a011e43e48f0d649430c9918
KarlTred265 Tred <tred26559@gmail.com>
2023-05-11 21:11:02 +0000

    add more

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=500)
+    image = CloudinaryField('image', null=True, blank=True)
+    body = models.TextField()
+    post_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/index.html', context)
+
+def detail(request, slug):
+    context = {}
+    return render(request, 'blog/detail.html', context)
+    
